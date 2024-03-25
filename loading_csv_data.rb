6ddec0cb5ead386e448ce97d75daa55da2aae3a1
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-03-25 03:57:02 -0400

    blocks

--- loading_csv_data.rb
+++ loading_csv_data.rb
@@ -1,18 +1,24 @@
 require "csv"
 
+=begin
 def classic_jazz?(album)
     album[2] == "Jazz" and album[3].to_i.between?(1950, 1959)
 end
+=end
 
 albums = CSV.read("albums.csv")[1..]
 
 selected_albums = []
 
+=begin
 for i in 0...albums.size do
     if classic_jazz?(albums[i])
         selected_albums << albums[i]
     end
 end
+=end
 
 #puts(albums)
-puts(selected_albums)
+#puts(selected_albums)
+
+puts(albums.select {|album| album[2] == "Jazz" and album[3].to_i.between?(1950, 1959)})

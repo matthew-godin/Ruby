b2459f6a3208a93a07a5c5a4c09cdab1b87f1ca2
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-03-26 01:18:51 -0400

    object oriented ruby

---
+++ use_dispenser.rb
@@ -0,0 +1,11 @@
+require "./dispenser.rb"
+
+office_dispenser = Dispenser.new
+
+puts(office_dispenser.dispense())
+puts(office_dispenser.dispense())
+puts(office_dispenser.dispense())
+puts(office_dispenser.dispense())
+puts(office_dispenser.dispense())
+puts(office_dispenser.dispense())
+puts(office_dispenser.dispense())
